210a3ee6644d0ef421669988c94d936c51938e24
cakeGit <cakeistastydev@gmail.com>
2024-10-13 12:02:23 +0100

    right click harvest util

--- src/main/java/com/ash/farm/AshEvents.java
+++ src/main/java/com/ash/farm/AshEvents.java
@@ -1,20 +1,49 @@
 package com.ash.farm;
 
 import com.ash.farm.access.MinecraftAccessor;
+import com.mojang.authlib.minecraft.client.MinecraftClient;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class AshEvents {
+    
+    @SubscribeEvent
+    public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
+        tryRightClickHarvest(event.getLevel(), event.getItemStack(), event.getPos(), event.getFace());
+    }
+    
+    public static void tryRightClickHarvest(Level level, ItemStack stack, BlockPos pos, Direction hitVecDirection) {
+        Minecraft mc = Minecraft.getInstance();
+        
+        if (!level.isClientSide) return;
+        if (mc.gameMode == null) return;
+        
+        if (!(stack.getItem() instanceof BlockItem blockItem)) return;
+        if (!(blockItem.getBlock() instanceof CropBlock heldCropBlock)) return;
+        
+        BlockState clickedState = level.getBlockState(pos);
+        
+        if (!(clickedState.getBlock() instanceof CropBlock cropBlock)) return;
+        if (heldCropBlock != cropBlock) return;
+        if (!cropBlock.isMaxAge(clickedState)) return;
+        
+        mc.gameMode.startDestroyBlock(pos, hitVecDirection);
+    }
     
     public static InteractionHand lastInteractionHand = null;
     public static InteractionHand lastUsedInteractionHand = InteractionHand.MAIN_HAND;
